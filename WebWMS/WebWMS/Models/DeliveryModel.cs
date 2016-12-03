c3d62499d97d1a9ebbe2736a4f6fa41cf6fa0ac0
George <George@DESKTOP-TEIVNGB>
2016-12-03 23:00:05 +0000

    Added new project to a new repository. Clone of XamarinWMS WebWMS app

---
+++ WebWMS/WebWMS/Models/DeliveryModel.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebWMS.Models
+{
+    public partial class DeliveryModel
+    {
+        public DeliveryModel()
+        {
+            DeliveryLines = new HashSet<DeliveryLineModel>();
+        }
+
+        [Key]
+        public int DeliveryId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ExpectedDate { get; set; }
+
+        public string Customer { get; set; }
+
+        public string State { get; set; }
+
+        public DateTime StateChangeTime { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<DeliveryLineModel> DeliveryLines { get; set; }
+
+    }
+}
