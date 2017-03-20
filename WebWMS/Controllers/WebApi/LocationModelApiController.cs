487ff0835aff0b00ae51d4af707c08dd1774c7ec
George <George@DESKTOP-TEIVNGB>
2017-03-20 18:57:08 +0000

    Edit Index files for Views. Added Rest Api for Location and Product

---
+++ WebWMS/Controllers/WebApi/LocationModelApiController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebWMS.Models;
+
+namespace WebWMS.Controllers.WebApi
+{
+    public class LocationModelApiController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/LocationModelApi
+        public IQueryable<LocationModel> GetLocationModels()
+        {
+            return db.LocationModels;
+        }
+
+        // GET: api/LocationModelApi/5
+        [ResponseType(typeof(LocationModel))]
+        public IHttpActionResult GetLocationModel(string id)
+        {
+            LocationModel locationModel = db.LocationModels.Find(id);
+            if (locationModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(locationModel);
+        }
+
+        // PUT: api/LocationModelApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLocationModel(string id, LocationModel locationModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != locationModel.LocationId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(locationModel).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocationModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/LocationModelApi
+        [ResponseType(typeof(LocationModel))]
+        public IHttpActionResult PostLocationModel(LocationModel locationModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.LocationModels.Add(locationModel);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (LocationModelExists(locationModel.LocationId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = locationModel.LocationId }, locationModel);
+        }
+
+        // DELETE: api/LocationModelApi/5
+        [ResponseType(typeof(LocationModel))]
+        public IHttpActionResult DeleteLocationModel(string id)
+        {
+            LocationModel locationModel = db.LocationModels.Find(id);
+            if (locationModel == null)
+            {
+                return NotFound();
+            }
+
+            db.LocationModels.Remove(locationModel);
+            db.SaveChanges();
+
+            return Ok(locationModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LocationModelExists(string id)
+        {
+            return db.LocationModels.Count(e => e.LocationId == id) > 0;
+        }
+    }
+}
---
+++ WebWMS/Controllers/WebApi/ProductModelApiController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebWMS.Models;
+
+namespace WebWMS.Controllers.WebApi
+{
+    public class ProductModelApiController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/ProductModelApi
+        public IQueryable<ProductModel> GetProductModels()
+        {
+            return db.ProductModels;
+        }
+
+        // GET: api/ProductModelApi/5
+        [ResponseType(typeof(ProductModel))]
+        public IHttpActionResult GetProductModel(string id)
+        {
+            ProductModel productModel = db.ProductModels.Find(id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productModel);
+        }
+
+        // PUT: api/ProductModelApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProductModel(string id, ProductModel productModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != productModel.ProdId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(productModel).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ProductModelApi
+        [ResponseType(typeof(ProductModel))]
+        public IHttpActionResult PostProductModel(ProductModel productModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ProductModels.Add(productModel);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductModelExists(productModel.ProdId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = productModel.ProdId }, productModel);
+        }
+
+        // DELETE: api/ProductModelApi/5
+        [ResponseType(typeof(ProductModel))]
+        public IHttpActionResult DeleteProductModel(string id)
+        {
+            ProductModel productModel = db.ProductModels.Find(id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            db.ProductModels.Remove(productModel);
+            db.SaveChanges();
+
+            return Ok(productModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductModelExists(string id)
+        {
+            return db.ProductModels.Count(e => e.ProdId == id) > 0;
+        }
+    }
+}
